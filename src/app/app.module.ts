4494c30a85d87c194c375aa9fa6d3120435e0597
annamati <anna101097@mail.ru>
2020-10-17 23:11:13 +0200

    dalsi component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FormsModule } from '@angular/forms';
 import { ServersComponent } from './servers/servers.component';
 import { HeaderComponent } from './header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
     HeaderComponent,
-    MainPageComponent
+    MainPageComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule,
