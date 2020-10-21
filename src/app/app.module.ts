7fbc294a865704826b563c8574c7037621a95100
annamati <anna101097@mail.ru>
2020-10-21 20:04:53 +0200

    3 stranky ktore boli otestovany v validatore a nemaju chyby-teraz isto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 import { FormsModule } from '@angular/forms';
-import { ServersComponent } from './servers/servers.component';
 import { HeaderComponent } from './header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
@@ -23,8 +21,6 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
     HeaderComponent,
     MainPageComponent,
     TopHeaderComponent,
